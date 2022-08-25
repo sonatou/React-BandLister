6ed0b94f7f72380e931d5bcc360ad0585d2318f0
sonatou <almeida_allan8@hotmail.com>
2022-08-25 14:48:34 -0300

    fixed indentation

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import { useState } from 'react';
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
@@ -13,25 +13,25 @@ let initialState = [      //setting the initialstate of the items usestate array
     "name": "metallica"
   },
 ];
-function App() {  
-    const [bands, setBands] = useState(initialState)    //creating useState to be able to change the content inside of it by using 'setBands'
+function App() {
+  const [bands, setBands] = useState(initialState)    //creating useState to be able to change the content inside of it by using 'setBands'
 
-    function addBand(e){        //method to add a new band
-      e.preventDefault();       // preventing the page reload as a default behaviour of the button
+  function addBand(e) {        //method to add a new band
+    e.preventDefault();       // preventing the page reload as a default behaviour of the button
 
-      const band = {
-        "id": document.getElementById('id').value,
-        "name": document.getElementById('name').value,
-      }
-
-      setBands([... bands, {... band}]); //setting the usestate copying the already existing items in 'bands' and addig a new band as a obj
+    const band = {
+      "id": document.getElementById('id').value,
+      "name": document.getElementById('name').value,
     }
+
+    setBands([...bands, { ...band }]); //setting the usestate copying the already existing items in 'bands' and addig a new band as a obj
+  }
   return (      //JSX starting
-    <> 
+    <>
       <form>
-        <input id='id' type='text' placeholder='Band Id'/>
-        <input id='name' type='text' placeholder='Band Name'/>
-        <button onClick={addBand}>Add Band</button> 
+        <input id='id' type='text' placeholder='Band Id' />
+        <input id='name' type='text' placeholder='Band Name' />
+        <button onClick={addBand}>Add Band</button>
       </form>
       <div className="mt-3">
         <ul className="list-group">
