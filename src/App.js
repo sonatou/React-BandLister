33d7f9bbec8dd643a47636a51437f8fef9c7e562
sonatou <almeida_allan8@hotmail.com>
2022-08-25 14:31:52 -0300

    made the logic to add a new item using 'useState'

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
+import {useState} from 'react';
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
+let initialState = [      //setting the initialstate of the items usestate array
+  {
+    "id": 1,
+    "name": "led zepellin"
+  },
+  {
+    "id": 2,
+    "name": "metallica"
+  },
+];
 function App() {  
-    const activity = [
-      {
-        "id": 1,
-        "description": "activity"
+    const [bands, setBands] = useState(initialState)    //creating useState to be able to change the content inside of it by using 'setBands'
+
+    function addBand(e){        //method to add a new band
+      e.preventDefault();       // preventing the page reload as a default behaviour of the button
+
+      const band = {
+        "id": document.getElementById('id').value,
+        "name": document.getElementById('name').value,
       }
-    ]
-  return (
-    <div className="App">
-      <header className="App-header">
-      <ul>
-        <li>{activity[0].id} {activity[0].description}</li>
-        <li onClick ={()=> console.log('working')}>Second Activity</li>
-        <li>Third Activity</li>
-        <li>Fourth Activity</li>
-      </ul>
-      </header>
-    </div>
+
+      setBands([... bands, {... band}]); //setting the usestate copying the already existing items in 'bands' and addig a new band as a obj
+    }
+  return (      //JSX starting
+    <> 
+      <form>
+        <input id='id' type='text' placeholder='Band Id'/>
+        <input id='name' type='text' placeholder='Band Name'/>
+        <button onClick={addBand}>Add Band</button> 
+      </form>
+      <div className="mt-3">
+        <ul className="list-group">
+          {bands.map(bnd => (
+            <li key={bnd.id} className="list-group-item">
+              {bnd.id} - {bnd.name}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
   );
 }
 
