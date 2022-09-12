701c79d6ae67d5cd4883053d44c2fd08cb2a932d
sonatou <almeida_allan8@hotmail.com>
2022-09-12 09:15:39 -0300

    Updates

--- front/README.md
+++
@@ -1,3 +0,0 @@
-### My first React + .NET Core app.
-
-This app is a Band Manager with a simple CRUD to list your favourite bands.
---
+++ precache-manifest.ba126554e90a0e779923973cefd84078.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "9ea2f300b4f0a551d74d",
+    "url": "/React.net-BandLister/static/css/main.fc9a615a.chunk.css"
+  },
+  {
+    "revision": "9ea2f300b4f0a551d74d",
+    "url": "/React.net-BandLister/static/js/main.d690ba92.chunk.js"
+  },
+  {
+    "revision": "7cb8a4a17ce9dfd5b8f6",
+    "url": "/React.net-BandLister/static/js/runtime~main.c61b7b69.js"
+  },
+  {
+    "revision": "a97f2242555844a8fbc2",
+    "url": "/React.net-BandLister/static/css/2.f72f6b8a.chunk.css"
+  },
+  {
+    "revision": "a97f2242555844a8fbc2",
+    "url": "/React.net-BandLister/static/js/2.a1adca50.chunk.js"
+  },
+  {
+    "revision": "9d2bbde416583cfa5cb30c995b41f732",
+    "url": "/React.net-BandLister/static/media/fa-brands-400.9d2bbde4.ttf"
+  },
+  {
+    "revision": "f417567703b4fcfbf750952bd2db8a44",
+    "url": "/React.net-BandLister/static/media/fa-regular-400.f4175677.ttf"
+  },
+  {
+    "revision": "f4bdd85a576812f553ec3cfea8b1a2b5",
+    "url": "/React.net-BandLister/static/media/fa-brands-400.f4bdd85a.woff2"
+  },
+  {
+    "revision": "d4a14b7a210bc8b5c2fe661a18e7e8db",
+    "url": "/React.net-BandLister/static/media/fa-v4compatibility.d4a14b7a.ttf"
+  },
+  {
+    "revision": "3e50e269ee627bb2279f91d18c085167",
+    "url": "/React.net-BandLister/static/media/fa-solid-900.3e50e269.woff2"
+  },
+  {
+    "revision": "dd5e5b90ca8ee986b13b04310f9aef9d",
+    "url": "/React.net-BandLister/static/media/fa-solid-900.dd5e5b90.ttf"
+  },
+  {
+    "revision": "83e2813a8fe0229d916c5b63fa9eab5e",
+    "url": "/React.net-BandLister/static/media/fa-regular-400.83e2813a.woff2"
+  },
+  {
+    "revision": "1f9db9971c2d69b78e7425a012a5e486",
+    "url": "/React.net-BandLister/static/media/fa-v4compatibility.1f9db997.woff2"
+  },
+  {
+    "revision": "38ab889707187206962d15a8b3bfcc47",
+    "url": "/React.net-BandLister/index.html"
+  }
+];
